8c70554a2de169099433c353c8f4ab3c72b1b190
Wintan <wintan1418@gmail.com>
2026-03-03 13:39:51 +0100

    Fix View Website link in admin to show public homepage
    - View Website now opens homepage in new tab (skips dashboard redirect)
    - Back to App still goes to dashboard as expected

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   layout false, only: [ :index ]
 
   def index
-    redirect_to dashboard_path if user_signed_in?
+    redirect_to dashboard_path if user_signed_in? && params[:preview].blank?
   end
 end
