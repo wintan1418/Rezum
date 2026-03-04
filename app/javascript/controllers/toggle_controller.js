f04f36538fe0d7de8624754c2c61b5c20c1cfcce
Wintan <wintan1418@gmail.com>
2026-03-04 12:41:05 +0100

    Add LinkedIn import option to resume wizard build-from-scratch page

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["content", "icon"]
+
+  toggle() {
+    this.contentTarget.classList.toggle("hidden")
+    if (this.hasIconTarget) {
+      this.iconTarget.classList.toggle("rotate-180")
+    }
+  }
+}
